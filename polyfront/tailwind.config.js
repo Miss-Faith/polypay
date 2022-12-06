0557ad0bb7a19286c7d3860fd7c886ce8100ec6d
Faith Mwangi <faith.mwangi@student.moringaschool.com>
2022-12-07 01:09:48 +0300

    update page style

--- polyfront/tailwind.config.js
+++ polyfront/tailwind.config.js
@@ -9,7 +9,20 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        'violet400': '#a78bfa',
+        'violet900': '#4c1d95',
+        'midnight': '#121063',
+        'metal': '#565584',
+        'tahiti': '#3ab7bf',
+        'silver': '#ecebff',
+        'bubble-gum': '#ff77e9',
+        'bermuda': '#78dcca',
+      },
+    },
   },
   plugins: [],
 }
